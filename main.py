12ad256c8a5a30d69673f4f25ec7ebe7cef31499
Harsh Gupta <harshguptashnd@gmail.com>
2020-09-30 20:31:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ r = requests.get('http://www.bing.com/search',params=params)
 soup = BeautifulSoup(r.text,'html.parser')
 results = soup.find('ol',{'id':'b_results'})
 links = results.findAll('li',{'class':'b_algo'})
-
+#this is the for loop
 for item in links:
     item_text = item.find('a').text
     item_href = item.find('a').attrs['href']
