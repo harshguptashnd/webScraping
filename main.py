c79e5333cb99d597f3e822ef88a658c4866d14b4
harshguptashnd <72147774+harshguptashnd@users.noreply.github.com>
2020-09-30 20:19:48 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+search = input('Enter search term:')
+params ={'q':search}
+r = requests.get('http://www.bing.com/search',params=params)
+
+soup = BeautifulSoup(r.text,'html.parser')
+results = soup.find('ol',{'id':'b_results'})
+links = results.findAll('li',{'class':'b_algo'})
+
+for item in links:
+    item_text = item.find('a').text
+    item_href = item.find('a').attrs['href']
+
+
+    if item_href or item_text:
+
+        print(item_text)
+        print(item_href)
+        print('parent',item.find('a').parent)
+        print('description', item.find('a').parent.parent.find('p').text)
+        children = item.children
+        for child in children:
+            print('child:', child)
+        children1 = item.find('h2')
+        print('next sibling is:',children1.next_sibling)
+print(soup.prettify())
+
